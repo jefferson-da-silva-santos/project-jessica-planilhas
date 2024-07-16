6abef28aabe1770e87dc21f4f4c6d7c9bfa10c24
jefferson-da-silva-santos <jeffrrWPG678@gmail.com>
2024-07-16 11:58:27 -0300

    corrigindo bug no menu mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+  window.onload = function () {
+    document.getElementById('inicio').scr
+  };
+
   let menuMobileVisible = false;
 
   const menuMobile = document.querySelector('#listMenu');
@@ -11,10 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
       menuMobile.style.display = 'flex';
       menuMobileVisible = !menuMobileVisible;
       document.body.style.overflow = 'hidden';
-
       iconBtnMenu.classList.remove('bi-list');
       iconBtnMenu.classList.add('bi-x-lg');
-      
+
     } else {
       menuMobile.style.display = 'none';
       menuMobileVisible = !menuMobileVisible;
@@ -23,6 +26,21 @@ document.addEventListener('DOMContentLoaded', () => {
       iconBtnMenu.classList.add('bi-list');
     }
   });
+
+
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > 900) {
+      menuMobile.style.display = 'flex';
+      document.body.style.overflow = 'auto';
+    } else {
+      menuMobile.style.display = 'none';
+      menuMobileVisible = false;
+      document.body.style.overflow = 'auto';
+      iconBtnMenu.classList.remove('bi-x-lg');
+      iconBtnMenu.classList.add('bi-list');
+    }
+  });
+
 
 
 });
